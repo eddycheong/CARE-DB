c46f200dd69482ea18dc267bb6f3bd97ea3f9bfa
MisterStabbeh <cheong.e49@gmail.com>
2013-11-06 18:27:32 -0800

    Implemented login to redirect to template if successful login

--- login.php
+++ login.php
@@ -4,27 +4,59 @@ include "db.php"; // Oracle login information
 // Server Code Implementation goes here
 // eg. Oracle login
 
-//===================
-// CONNECT TO ORACLE
-//===================
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+	$uname = $_POST['usr'];
+	$pword = $_POST['pwd'];
 
-if($c = oci_connect($ora_usr, $ora_pwd, "ug")) {
-	// Successfully connected to oracle
-	echo "Connected to Oracle\n";
+	$errmsg = "";
 
-	// Testing Oracle Queries
-	$s = oci_parse($c, 'select * from players');
-	oci_execute($s);
-	oci_fetch_all($s, $res);
-	//echo "<pre>\n";
-	var_dump($res);
-	//echo "</pre>\n";	
+	//$uname = htmlspecialchars($uname);
+	//$pword = htmlspecialchars($pword);
 
-	oci_close($c);
-} else {
-	$err = oci_error();
-	echo "Oracle Connect Error " . $err['message'];
-} 
+	//===================
+	// CONNECT TO ORACLE
+	//===================
+
+	if($c = oci_connect($ora_usr, $ora_pwd, "ug")) {
+		// Successfully connected to oracle
+
+		// Check if username and password match in either
+		// employee table or doctor table. No duplicates.
+		$query = "select loginid
+			 from employee
+			 where loginid = '". $uname ."' and
+			 password = '". $pword ."'
+			 union
+			 select loginid
+	 		 from doctor
+			 where loginid = '". $uname ."' and
+			 password = '". $pword ."'";
+		
+		$s = oci_parse($c, $query);
+		oci_execute($s);
+		oci_fetch_all($s, $res);
+
+		$check = oci_num_rows($s);
+		oci_free_statement($s);
+
+		oci_close($c);
+	} else {
+		$err = oci_error();
+		echo "Oracle Connect Error " . $err['message'];
+	} 
+
+	// Check if username and password matched with anything from oracle
+	if ($check == 1) {
+		// A match in the database. Valid login
+		// TODO: Save the session
+		header ("Location: template.php");
+			
+	} else {
+		// No match found from the database
+		// Incorrect username or password
+		$errmsg = "Invalid login. Username or password is incorrect";
+	}	
+}
 ?>
 <html>
 <head>
@@ -34,7 +66,10 @@ if($c = oci_connect($ora_usr, $ora_pwd, "ug")) {
 <body>
 
 <center>
-	<form name = "" method = "post" action ="template.php">
+	<?php
+		echo $errmsg, "<br>";
+	?>
+	<form name = "" method = "post" action = "<?php echo $_SERVER['PHP_SELF']; ?>" >
 		<input type = "text" name ="usr" value"">
 		<br>
 		<input type = "password" name = "pwd" value "" maxlength="16">
