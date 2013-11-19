f27f3000444c656f50a6008318369598fbcfbd5e
Cindy Lee <anacindy@live.ca>
2013-11-19 13:42:45 -0800

    appAddPatient completed
    - I will complete the getRandomPid() function later

--- appAddPatient.php
+++ appAddPatient.php
@@ -9,7 +9,7 @@ ini_set('session.save_path', realpath(dirname($_SERVER['DOCUMENT_ROOT']) . '/../
 session_start();
 
 // If no one is logged in, redirect them to the login page
-if(!(isset($_SESSION['login']) || $_SESSION['login'] == '')) {
+if(!(isset($_SESSION['login']) || $_SESSION['login'] != '')) {
 	header("Location: login.php");
 }
 
@@ -24,17 +24,17 @@ echo $utype;
 // chmod 755 newpage.php
 
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
+	$pname = $_POST["pname"];
+	$address = $_POST["address"];
+	$phone = $_POST["phone"];
+	$email = $_POST["email"];
+	$carecard = $_POST["carecard"];
 
-	echo "Pass";
-	$info = $_POST['addpatient'];
-	echo $info;
-	// Obtain the search statement
-	$search = $_POST['search'];
-	//echo $search;
-
-	// Break down the string into pieces
-	$pieces = explode(" ", $search);
-	$n_pieces = sizeof($pieces);
+	$pid = rand(1000, 9999);
+	//$pid = getRandomPid();
+	
+	$compare = $pname != null && $address != null && $phone != null && $email != null;
+	if($compare && $carecard != null) {
 
 	//===================
 	// CONNECT TO ORACLE
@@ -42,66 +42,61 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
 	if ($c = oci_connect ($ora_usr, $ora_pwd, "ug")) {
 
 		// Template search query, replace table and attribute
-		$query = searchByParts($n_pieces, $pieces);
-		$s = oci_parse($c, $query);
-		oci_execute($s);
 		
-		//Oracle Fetches
-		$n_rows = oci_fetch_all($s, $res, null, null, OCI_FETCHSTATEMENT_BY_ROW);
-		oci_close($c);
+			
+			$query = "insert into patient values (".$pid.", '".$pname."', '".$address."', '".$phone."', '".$email."', '".$carecard."')";
+			$s = oci_parse($c, $query);
+			oci_execute($s);
+			oci_close($c);
+			header("Location: appConfirm.php?pname=". $pname."&phone=".$phone);
+		
 	} else {
 		$err = oci_error();
 		echo "Oracle Connect Error " . $err['message'];
 	}
+	}
 }
-
+/* WILL WORK ON THIS LATER
+function getRandomPid(){
+	$pid = rand(1000, 9999);
+	
+	for($i=0; $i<$n_rows; $i++){
+		if($pids[$i] == $pid)
+			getRandomPid();
+		}
+		echo "working";		
+		return $pid;
+	}
+}
+*/
 ?>
 
 <!--Design the page below-->
 <html>
 <head>
-	<title>Template</title>
+	<title>Add Patient Information</title>
 	<link rel = "stylesheet" type = "text/css" href= "./styles/styling.css">
 </head>
 <body style = "text-align: center;">
 	<div id = "header">
-		<h1 style = "margin-bottom: 0;"> Template </h1>
+		<h1 style = "margin-bottom: 10;"> New Patient </h1>
 	</div>
-	<!--
-	<div id = "side-panel">
-	<?php
-		// assign arr based on user type
-		
-		//buildSideLink($arr);
-	?>
-	</div>
-	-->
-
+	<!--<div>
+	<a href="appConfirm.php"> <input type = "submit" name = "submit" value = "Submit"></a>
+	</div>-->
 	<div id = "content">
-		Content appears here
-		<form id = "search" name "" method= "post">
-			<input type = text name = "search" value "">
+		<form id = "insert" method= "post">
+			<label for="name">Name:</label>   <input type="text" name="pname" value "" /><br/>
+			<label for="address">Address:</label>   <input type="text" name="address" value "" size = "30"/><br/>
+			<label for="phone">Phone#:</label>   <input type="text" name="phone" value "" /><br/>
+			<label for="email">Email:</label>   <input type="text" name="email" value "" /><br/>
+			<label for="carecard">Carecard#:</label>   <input type="text" name="carecard" value "" /><br/>
+			
+			<input type = "submit" name = "submit" value = "Submit">
 		</form>
-
-		<?php
-		// sample code to use result from search
-			echo '<center>';
-			echo '<table border="1">';
-			echo '<tr>';
-			echo '<th>EID</th>';
-			echo '<th>Employee Name</th>';
-			echo '</tr>';
-			for($i = 0; $i < $n_rows; $i++) {
-				echo '<tr>';
-				echo '<th>'. $res[$i]['EID'] .'</th>';
-				echo '<th>'. $res[$i]['ENAME'] .'</th>';
-				echo '</tr>';
-			}
-			echo '</table>';
-			echo '</center>';
-		?>
-
+		
 	</div>
+	
 <!-- Need to learn divs, work on UI later-->
 <!--	<div id = "leftMargin">
 	</div>
