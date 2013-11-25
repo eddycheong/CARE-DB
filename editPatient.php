88724f044935228ff48e4f8d6c75b37a604ab411
MisterStabbeh <cheong.e49@gmail.com>
2013-11-25 10:27:48 -0800

    editProfile works, just need type and length checking

--- editPatient.php
+++ editPatient.php
@@ -21,7 +21,6 @@ if(!(isset($_SESSION['login']) || $_SESSION['login'] != '')) {
 
 $pid = $_REQUEST['pid'];
 
-/*
 //===================
 // CONNECT TO ORACLE
 //===================
@@ -36,13 +35,11 @@ if ($c = oci_connect ($ora_usr, $ora_pwd, "ug")) {
 	oci_execute($s);
 	$res = oci_fetch_array($s, OCI_BOTH);
 	
-	oci_commit($c);
 	oci_close($c);		
 } else {
 	$err = oci_error();
 	echo "Oracle Connect Error " . $err['message'];
 }
-*/
 
 if(($_SERVER['REQUEST_METHOD'] == 'POST') && isset($_POST['update'])) {
 	$pname = $_POST["pname"];
@@ -93,7 +90,7 @@ if(($_SERVER['REQUEST_METHOD'] == 'POST') && isset($_POST['update'])) {
 		//echo "add carecard<br>";
 	}
 		
-	$query .= "where pid =".$pid;
+	$query .= "where pid = ".$pid;
 
 	//===================
 	// CONNECT TO ORACLE
@@ -102,9 +99,9 @@ if(($_SERVER['REQUEST_METHOD'] == 'POST') && isset($_POST['update'])) {
 
 		// Template search query, replace table and attribute	
 			echo $query."<br>";
+			
 			$s = oci_parse($c, $query);
-			oci_execute($s);
-			oci_free_statement($s);
+			$r = oci_execute($s);
 			
 			oci_commit($c);
 			oci_close($c);
@@ -154,27 +151,27 @@ function getRandomPid(){
 			<table class = "addedit">
 				<tr>
 					<th><label for="name">Name:</label></th>  
-					<td><input type="text" name="pname" value = "<?php //echo $res['PNAME'];?>"></td>
+					<td><input type="text" name="pname" value = "<?php echo $res['PNAME'];?>"></td>
 				</tr>
 				<tr>
 					<th><label for="address">Address:</label></th>
-					<td><input type="text" name="address" value = "<?php //echo $res['ADDRESS'];?>" size = "30"/></td>
+					<td><input type="text" name="address" value = "<?php echo $res['ADDRESS'];?>" size = "30"/></td>
 				</tr>
 				<tr>
 					<th><label for="phone">Phone#:</label></th>		
-					<td> <input type="text" name="phone" value = "<?php //echo $res['PHONE'];?>"></td>
+					<td> <input type="text" name="phone" value = "<?php echo $res['PHONE'];?>"></td>
 				</tr>
 				<tr>
 					<th><label for="email">Email:</label></th>  
-					<td><input type="text" name="email" value ="<?php //echo $res['EMAIL'];?>" ></td>
+					<td><input type="text" name="email" value ="<?php echo $res['EMAIL'];?>" ></td>
 				</tr>
 				<tr>
 					<th><label for="carecard">Carecard#:</label></th>   
-					<td><input type="text" name="carecard" value ="<?php //echo $res['CARECARD'];?>" ></td>
+					<td><input type="text" name="carecard" value ="<?php echo $res['CARECARD'];?>" ></td>
 				</tr>
 	</table>
 				<input type = "hidden" name = "update" value = true>
-				<input type = "hidden" name = "pid" value = "<?php //echo $res['PID'];?>">
+				<input type = "hidden" name = "pid" value = "<?php echo $res['PID'];?>">
 				<input id = "search" type = "submit" name = "submit" value = "Submit">
 		</form>
 		
@@ -182,4 +179,4 @@ function getRandomPid(){
 
 	<div id = "footer"></div>
 </body>
-</html>
+</html
